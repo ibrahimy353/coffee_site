9dbd1829b2252ec6be680c699b58ef3427aa86c2
ibrahimy353 <ihusssein@gmail.com>
2022-04-06 15:39:18 +0300

    added to project

--- js/script.js
+++ js/script.js
@@ -13,3 +13,11 @@ document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
